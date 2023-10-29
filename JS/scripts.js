61f5573259d77bece5aecb1f8a0c748fdeb7b19d
BalancedProtector <Stevemartin44224@gmail.com>
2023-10-29 14:48:36 -0400

    The first starter pokemon trio

---
+++ JS/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name:'Bulbasaur', height: '2\' 04\"', types['Grass','poison']}
+    {name:'Charmander', height: '2\'', types['Fire']}
+    {name:'Squirtle', height: '1\' 08\"', types['Water']}
+] // Type format = name: 'string'; height: number; types: [arrayItem1, arrayItem2];
