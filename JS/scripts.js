c60aafff16b8c04817a78cc45b1991b05269de98
BalancedProtector <Stevemartin44224@gmail.com>
2023-11-03 03:18:01 -0400

    Problematic Change Will Undo Later
    Having syntax issues, scheduled meeting with Mentor to discuss why and possible fixes

--- JS/scripts.js
+++ JS/scripts.js
@@ -3,3 +3,7 @@ let pokemonList = [
     {name:'Charmander', height: '2\'', types:['Fire']},
     {name:'Squirtle', height: '1\' 08\"', types:['Water']},
 ] // Type format = name: 'string'; height: number; types: [arrayItem1, arrayItem2];
+let i = null;
+for (i = 0, i < pokemonList.length, i++) {
+    document.write(pokemonList[i]);
+}
