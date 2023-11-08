712623cad4c9cfd0b41084f2b5e5f07e614c9819
BalancedProtector <Stevemartin44224@gmail.com>
2023-11-08 01:50:51 -0500

    Re-structured for better referencing

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,9 +1,17 @@
 let pokemonList = [
-    {name:'Bulbasaur', height: '2\' 04\"', types:['Grass','poison']},
-    {name:'Charmander', height: '2\'', types:['Fire']},
-    {name:'Squirtle', height: '1\' 08\"', types:['Water']},
+    {name:'Bulbasaur', 
+    height: '2\' 04\"', 
+    types:['Grass','poison']
+    },
+    {name:'Charmander', 
+    height: '2\'', 
+    types:['Fire']
+    },
+    {name:'Squirtle', 
+    height: '1\' 08\"', 
+    types:['Water']
+    },
 ] // Type format = name: 'string'; height: number; types: [arrayItem1, arrayItem2];
-let i = null;
 for (i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i]);
+    document.write(pokemonList[i.name]);
 }
