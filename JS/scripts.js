8f0084e99de0b2ddd9e272ef7fa48c61941a1d99
BalancedProtector <139424334+BalancedProtector@users.noreply.github.com>
2023-12-18 05:39:02 -0500

    IIFE Update excercise 1.5 pt 1
    Learned IIFE things and implemented them.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,29 +1,35 @@
-let pokemonList = [
-    {
-    name:'Bulbasaur', 
-    height: '0.7', 
-    types:['Grass','poison']
-    },
-    {
-    name:'Charmander', 
-    height: '1.7', 
-    types:['Fire']
-    },
-    {
-    name:'Squirtle', 
-    height: '1', 
-    types:['Water']
-    },
-] // Type format = name: 'string'; height: number; types: [arrayItem1, arrayItem2];
-function printArrayDetails() {
-    for (i = 0; i < pokemonList.length; i++) {
-        document.write(pokemonList[i].name + ' (height in meters: ' + pokemonList[i].height + ') ');
-        if (pokemonList[i].height > '1') {
-            document.write(' - Wow, that\'s a big one! ');
-        }
-        document.write('<br>');
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+        name:'Bulbasaur', 
+        height: '0.7', 
+        types:['Grass','poison']
+        },
+        {
+        name:'Charmander', 
+        height: '1.7', 
+        types:['Fire']
+        },
+        {
+        name:'Squirtle', 
+        height: '1', 
+        types:['Water']
+        },
+    ] // Type format = name: 'string'; height: number; types: [arrayItem1, arrayItem2];
+    function getAll() {
+        return pokemonList;
     }
-}
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+    return {
+        getAll: getAll,
+        add: add
+    };
+
+})();
+
+console.log(pokemonRepository.getAll());
 /*
 pokemonList.forEach(pokemon => {
     document.write(pokemon.name + ' (height in meters: ' + pokemon.height + ') ');
