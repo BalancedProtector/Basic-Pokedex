f49bd0dad45870c72f7ed4a9923190d120a4ef9c
BalancedProtector <139424334+BalancedProtector@users.noreply.github.com>
2024-02-01 15:30:54 -0500

    Indentation and spacing update

--- JS/scripts.js
+++ JS/scripts.js
@@ -17,69 +17,56 @@ let pokemonRepository = (function() {
     }
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function() {
-                let modalContainer = document.querySelector("#pokedexInfo");
-                modalContainer.innerHTML = "";
-
-                let modal = document.createElement("div");
-                modal.classList.add("modal");
-
-                let closeButtonElement = document.createElement("button");
-                closeButtonElement.classList.add("modal-close");
-                closeButtonElement.innerText = "X";
-                closeButtonElement.addEventListener("click", hideModal);
-
-                let titleElement = document.createElement("h1");
-                titleElement.innerText = pokemon.name;
-
-                let contentElement = document.createElement("p");
-                contentElement.innerText = "Height: " + pokemon.height;
-
-                let imageElement = document.createElement("img");
-                imageElement.src = pokemon.imageUrl;
-
-                modal.appendChild(closeButtonElement);
-                modal.appendChild(titleElement);
-                modal.appendChild(contentElement);
-                modal.appendChild(imageElement);
-
-                modalContainer.appendChild(modal);
-                modalContainer.classList.add("is-visible");
-
-                document.querySelector("#pokedexEntry").addEventListener("click", function(e) {
-                    let target = e.target;
-                    if (target === modalContainer) {
-                        hideModal();
-                    }
-                });
-
-                document.addEventListener("keydown", e => {
-                    if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
-                        hideModal();
-                    }
-                });
-
-                function hideModal() {
-                    modalContainer.classList.remove("is-visible");
+            let modalContainer = document.querySelector("#pokedexInfo");
+            modalContainer.innerHTML = "";
+            let modal = document.createElement("div");
+            modal.classList.add("modal");
+            let closeButtonElement = document.createElement("button");
+            closeButtonElement.classList.add("modal-close");
+            closeButtonElement.innerText = "X";
+            closeButtonElement.addEventListener("click", hideModal);
+            let titleElement = document.createElement("h1");
+            titleElement.innerText = pokemon.name;
+            let contentElement = document.createElement("p");
+            contentElement.innerText = "Height: " + pokemon.height;
+            let imageElement = document.createElement("img");
+            imageElement.src = pokemon.imageUrl;
+            modal.appendChild(closeButtonElement);
+            modal.appendChild(titleElement);
+            modal.appendChild(contentElement);
+            modal.appendChild(imageElement);
+            modalContainer.appendChild(modal);
+            modalContainer.classList.add("is-visible");
+            document.querySelector("#pokedexEntry").addEventListener("click", function(e) {
+                let target = e.target;
+                if (target === modalContainer) {
+                    hideModal();
                 }
+            });
+            document.addEventListener("keydown", e => {
+                if (e.key === "Escape" && modalContainer.classList.contains("is-visible")) {
+                    hideModal();
+                }
+            });
+            function hideModal() {
+                modalContainer.classList.remove("is-visible");
+            }
         });
     }
     function addListItem(pokemon) {
         let pokemonList = document.querySelector(".pokemonList");
         let listpokemon= document.createElement("li");
         let button = document.createElement("button");
-
         button.innerText = pokemon.name;
         button.classList.add("pokedexEntry");
         button.addEventListener("click", function(event){
             showDetails(pokemon);
         });
-
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
         button.addEventListener("click", function(event) {
             showDetails(pokemon);
         });
-        
     }
     function loadList() {
         return fetch(apiUrl).then(function(response){
